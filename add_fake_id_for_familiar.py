4d7a75e1006c6f5247691d5793fc4423fb95f751
lqq <1042915345@qq.com>
2025-04-28 13:54:28 +0800

    modified familiar

--- add_fake_id_for_familiar.py
+++ add_fake_id_for_familiar.py
@@ -16,7 +16,7 @@ with open('./data/train_familiar/cf_fake_id_class.json','w') as file:
     json.dump(raw_data, file, indent=4) 
 
 
-data_dir = './data/edit_data/zsre_gptj_edit.json'
+data_dir = './data/edit_data/gptj_zsre_edit.json'
 with open(data_dir, "r") as f:
     raw_data = json.load(f)
 raw_data = raw_data[0:10000]
@@ -28,7 +28,7 @@ with open('./data/train_familiar/zsre_gptj_fake_id_class.json','w') as file:
     json.dump(raw_data, file, indent=4) 
 
 
-data_dir = './data/edit_data/zsre_phi2_edit.json'
+data_dir = './data/edit_data/phi2_zsre_edit.json'
 with open(data_dir, "r") as f:
     raw_data = json.load(f)
 raw_data = raw_data[0:10000]
